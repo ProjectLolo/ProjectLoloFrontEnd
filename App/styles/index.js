4efa7fede11d0e98f10c011b1021091a79b5c6b3
anpospisil <anpospisil89@gmail.com>
2020-11-03 15:17:48 +0100

    created a global style sheet

---
+++ App/styles/index.js
@@ -0,0 +1,26 @@
+import { StyleSheet } from 'react-native';
+
+export default StyleSheet.create({
+  purple: {
+    backgroundColor: "#6464E1"
+  },
+  ltPurple: {
+    backgroundColor: "#AEADFF"
+  },
+  teal: {
+    backgroundColor: "#11E9E0"
+  },
+  yellow: {
+    backgroundColor: "#FFD33D"
+  },
+  ltPink: {
+    backgroundColor: "#F6E6CC"
+  },
+  pink: {
+    backgroundColor: "#F6ABA7"
+  },
+  dkPink: {
+      backgroundColor: "#FF6E5A"
+  }
+
+});
