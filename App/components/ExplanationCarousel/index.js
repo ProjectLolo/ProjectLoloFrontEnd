3439eb8157b9446a9866ef6262e0e19ad9d3f5da
DiegoOTdC <studioartdiego@gmail.com>
2020-11-03 18:28:39 +0100

    install react-native-carousel dep.

--- App/components/ExplanationCarousel/index.js
+++ App/components/ExplanationCarousel/index.js
@@ -1,7 +1,10 @@
-import React from 'react'
+import React from "react";
+import { View, Text, Image, SafeAreaView, FlatList } from "react-native";
+import images from "@assets/images";
 
 export default function ExplanationCarousel() {
-    return (
-       
-    )
+  //this is the entire carousel NOT just one card.
+  //we could add all info for the cards here... I guess...
+
+  return <View></View>;
 }
--- App/screens/Welcome/index.js
+++ App/screens/Welcome/index.js
@@ -2,16 +2,16 @@ import React from "react";
 import { View, Text, TouchableWithoutFeedback, Image } from "react-native";
 import images from "@assets/images";
 import styles from "@styles/styles";
+import ExplanationCarousel from "../../components/ExplanationCarousel";
 
 export default function Welcome({ navigation }) {
   return (
-    <View style={{ flex: 1 }}>
+    <View style={{ flex: 1, alignItems: "center" }}>
       <Image style={styles.peekabooLogo} source={images.peekabooLogo} />
-      <Text style={{ textAlign: "center" }}>Welcome</Text>
+      <ExplanationCarousel />
+      <Text>Welcome</Text>
       <TouchableWithoutFeedback onPress={() => navigation.navigate("Login")}>
-        <Text style={{ textAlign: "center", marginTop: 50 }}>
-          Press here to continue
-        </Text>
+        <Text style={{ marginTop: 50 }}>Press here to continue</Text>
       </TouchableWithoutFeedback>
     </View>
   );
