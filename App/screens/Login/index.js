eaecb963e085e6fcce5a35ebd5c016ef91b67e39
DiegoOTdC <studioartdiego@gmail.com>
2020-11-04 23:20:18 +0100

    add show/hide password button

--- App/screens/Login/index.js
+++ App/screens/Login/index.js
@@ -19,9 +19,53 @@ export default function Login({ navigation }) {
     hidePassword ? setHidePassword(false) : setHidePassword(true);
   }
 
+  const styles = StyleSheet.create({
+    showPassword: {
+      textAlign: "right",
+      fontSize: 16,
+      right: "10%",
+      marginTop: 5,
+    },
+    top: { marginTop: "10%" },
+    loginTitle: { marginTop: "20%", marginBottom: "5%" },
+    login: {
+      width: "90%",
+      padding: "4.5%",
+      alignSelf: "center",
+      borderRadius: 5,
+    },
+    inputTitle: {
+      marginLeft: "5%",
+      paddingBottom: 5,
+      paddingTop: "5%",
+    },
+    inputEmail: {
+      alignSelf: "center",
+      textAlign: "left",
+      width: "90%",
+      height: 60,
+      fontSize: 16,
+      paddingLeft: 15,
+      borderWidth: 0.2,
+      borderColor: "grey",
+      borderRadius: 5,
+    },
+    inputPassword: {
+      alignSelf: "center",
+      width: "90%",
+      height: 60,
+      paddingLeft: 15,
+      paddingTop: 18,
+      borderWidth: 0.2,
+      borderColor: "grey",
+      borderRadius: 5,
+      flexDirection: "row",
+    },
+  });
+
   return (
-    <TouchableWithoutFeedback style={{ flex: 1 }} onPress={Keyboard.dismiss}>
-      <View>
+    <TouchableWithoutFeedback onPress={Keyboard.dismiss}>
+      <View style={{ flex: 1 }}>
         <CText
           h4
           bold
@@ -32,21 +76,38 @@ export default function Login({ navigation }) {
         />
         <CText p regular black title={"Email"} style={styles.inputTitle} />
         <TextInput
-          style={[styles.input, style.regular]}
+          style={[styles.inputEmail, style.regular]}
           placeholder="Enter email..."
           placeholderTextColor="grey"
           onChangeText={(text) => setEmail(text)}
           value={email}
         />
         <CText p regular black title={"Password"} style={styles.inputTitle} />
+
         <TextInput
-          style={[styles.input, style.regular]}
+          style={[
+            styles.inputPassword,
+            style.regular,
+            { textAlign: "left", fontSize: 16 },
+          ]}
           placeholder="Enter password..."
           placeholderTextColor="grey"
           secureTextEntry={hidePassword}
           onChangeText={(text) => setPassword(text)}
           value={password}
         />
+        {password === "" ? null : (
+          <TouchableWithoutFeedback onPress={togglePassword}>
+            <CText
+              p
+              semiBold
+              grey
+              title={hidePassword ? "Show password" : "Hide password"}
+              style={styles.showPassword}
+            />
+          </TouchableWithoutFeedback>
+        )}
+
         <TouchableWithoutFeedback
           onPress={() => navigation.navigate("SignUp")} //onPress should dispatch info to backend, to get Token in Redux. Then App.js should switch to the other StackNavigator.
         >
@@ -73,30 +134,3 @@ export default function Login({ navigation }) {
     </TouchableWithoutFeedback>
   );
 }
-
-const styles = StyleSheet.create({
-  top: { marginTop: "10%" },
-  loginTitle: { marginTop: "20%", marginBottom: "5%" },
-  login: {
-    width: "90%",
-    padding: "4.5%",
-    alignSelf: "center",
-    borderRadius: 5,
-  },
-  inputTitle: {
-    marginLeft: "5%",
-    paddingBottom: 5,
-    paddingTop: "5%",
-  },
-  input: {
-    alignSelf: "center",
-    textAlign: "left",
-    width: "90%",
-    height: "10%",
-    fontSize: 16,
-    paddingLeft: 15,
-    borderWidth: 0.2,
-    borderColor: "grey",
-    borderRadius: 5,
-  },
-});
