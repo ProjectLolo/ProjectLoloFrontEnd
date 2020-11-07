63f13230643c261e63e5d628afba6f4817993d07
DiegoOTdC <studioartdiego@gmail.com>
2020-11-07 15:21:10 +0100

    add styling to lovebank-cards

--- App/components/MediaContentCard/index.js
+++ App/components/MediaContentCard/index.js
@@ -2,12 +2,43 @@ import React from "react";
 import { View, Text, Image } from "react-native";
 import styles from "@styles/styles";
 import images from "@assets/images";
+import colors from "@assets/colors";
 
-export default function MediaContentCard() {
+export default function MediaContentCard(props) {
+  const { title, person, topColor, bottomColor } = props;
   return (
-    <View style={styles.cardContainer}>
-      <Text>I'm a card!</Text>
-      <View style={{ backgroundColor: "red" }}>
+    <View
+      style={[
+        styles.cardContainer,
+        { borderRadius: 20, width: 180, height: 120, marginTop: 30 },
+      ]}
+    >
+      <View
+        style={{
+          backgroundColor: colors[topColor],
+          borderTopLeftRadius: 20,
+          borderTopRightRadius: 20,
+          height: 35,
+          justifyContent: "center",
+        }}
+      >
+        <Text
+          style={[styles.cardTitle, { paddingHorizontal: 5 }]}
+          adjustsFontSizeToFit={true}
+          numberOfLines={1}
+        >
+          {title} by {person}
+        </Text>
+      </View>
+      <View
+        style={{
+          backgroundColor: colors[bottomColor],
+          justifyContent: "center",
+          borderBottomLeftRadius: 20,
+          borderBottomRightRadius: 20,
+          padding: 5,
+        }}
+      >
         <Image style={styles.cardImage} source={images.videoCameraPurple} />
       </View>
     </View>
--- App/screens/LoveBank/index.js
+++ App/screens/LoveBank/index.js
@@ -1,16 +1,66 @@
 import React from "react";
-import { View, Text, TouchableWithoutFeedback } from "react-native";
+import { View, Text, TouchableWithoutFeedback, FlatList } from "react-native";
 import NavHome from "../../components/NavHome";
 import NavButtons from "../../components/NavButtons";
 import MediaContentCard from "../../components/MediaContentCard";
 import styles from "@styles/styles";
 
 export default function LoveBank({ navigation }) {
+  const cardContent = [
+    {
+      id: 1,
+      title: "Kid goes to bed story",
+      person: "dad",
+      topColor: "pink",
+      bottomColor: "teal",
+    },
+    {
+      id: 2,
+      title: "Song",
+      person: "auntie Annie",
+      topColor: "purple",
+      bottomColor: "yellow",
+    },
+    {
+      id: 3,
+      title: "Animal kingdom",
+      person: "cousin Jan",
+      topColor: "teal",
+      bottomColor: "ltPurple",
+    },
+    {
+      id: 4,
+      title: "Life lesson",
+      person: "grammy Lis",
+      topColor: "yellow",
+      bottomColor: "ltPink",
+    },
+  ];
+
   return (
     <View style={{ flex: 1, justifyContent: "space-evenly" }}>
       <NavHome />
       <Text style={styles.titleText}>Welcome to [kid]'s love bank!</Text>
-      <MediaContentCard />
+      <FlatList
+        contentContainerStyle={{
+          alignSelf: "center",
+        }}
+        data={cardContent}
+        numColumns={2}
+        keyExtractor={(item) => item.id}
+        renderItem={({ item }) => {
+          return (
+            <TouchableWithoutFeedback>
+              <MediaContentCard
+                title={item.title}
+                person={item.person}
+                topColor={item.topColor}
+                bottomColor={item.bottomColor}
+              />
+            </TouchableWithoutFeedback>
+          );
+        }}
+      />
       <TouchableWithoutFeedback onPress={() => navigation.navigate("Settings")}>
         <Text style={{ textAlign: "center", marginTop: 50 }}>
           Press here to go to Settings
