bde1df00cd5b216d6ebdbd46e239e19daf3a426c
DiegoOTdC <studioartdiego@gmail.com>
2020-11-08 14:52:33 +0100

    add comment input box + buttons

--- App/screens/MediaContentDetails/index.js
+++ App/screens/MediaContentDetails/index.js
@@ -1,19 +1,23 @@
-import React from "react";
+import React, { useState } from "react";
 import {
   View,
   Text,
   TouchableWithoutFeedback,
   Image,
   FlatList,
+  TextInput,
 } from "react-native";
 import NavHome from "../../components/NavHome";
 import MediaContentComments from "../../components/MediaContentComments";
 import styles from "@styles/styles";
 import colors from "@assets/colors";
 import images from "@assets/colors";
+import fonts from "@assets/fonts";
+import { Colors } from "react-native/Libraries/NewAppScreen";
 
 export default function MediaContentDetails({ route, navigation }) {
   const { title, person, topColor, bottomColor, video } = route.params;
+  const [comment, setComment] = useState("");
 
   const cardContent = [
     {
@@ -68,6 +72,127 @@ export default function MediaContentDetails({ route, navigation }) {
           }}
         >
           <Image style={styles.cardImage} source={video} />
+        </View>
+      </View>
+
+      <View
+        style={{
+          backgroundColor: colors.ltPurple,
+          marginHorizontal: 10,
+          width: "95%",
+          borderRadius: 25,
+        }}
+      >
+        <View
+          style={{
+            backgroundColor: colors.purple,
+            borderTopLeftRadius: 25,
+            borderTopRightRadius: 25,
+          }}
+        >
+          <Text style={{ alignSelf: "center", color: "white" }}>
+            NameOfPersonWhoIsLoggedIn
+          </Text>
+        </View>
+        <View
+          style={{
+            flexDirection: "row",
+            width: "100%",
+            justifyContent: "space-between",
+          }}
+        >
+          <TextInput
+            style={{
+              color: colors.purple,
+              margin: 10,
+              width: "60%",
+            }}
+            placeholder='"Write comment here..."'
+            placeholderTextColor={colors.purple}
+            multiline={true}
+            onChangeText={(text) => setComment(text)}
+            value={comment}
+          />
+          <View
+            style={{
+              flexDirection: "column",
+              justifyContent: "space-between",
+              alignItems: "center",
+              backgroundColor: colors.purple,
+              width: "30%",
+              borderBottomRightRadius: 25,
+            }}
+          >
+            <TouchableWithoutFeedback>
+              <View
+                style={{
+                  backgroundColor: colors.ltPurple,
+                  borderRadius: 25,
+                  padding: 10,
+                  margin: 10,
+                  width: "90%",
+                }}
+              >
+                <Text
+                  style={{
+                    alignSelf: "center",
+                    color: "white",
+                    fontFamily: fonts.bold,
+                  }}
+                >
+                  Record
+                </Text>
+                <Text
+                  style={{
+                    alignSelf: "center",
+                    color: "white",
+                    fontFamily: fonts.bold,
+                  }}
+                >
+                  video
+                </Text>
+                <Text
+                  style={{
+                    alignSelf: "center",
+                    color: "white",
+                    fontFamily: fonts.bold,
+                  }}
+                >
+                  message
+                </Text>
+              </View>
+            </TouchableWithoutFeedback>
+            <TouchableWithoutFeedback>
+              <View
+                style={{
+                  backgroundColor: colors.ltPurple,
+                  borderRadius: 25,
+                  padding: 10,
+                  margin: 10,
+                  width: "90%",
+                }}
+              >
+                <Text
+                  style={{
+                    alignSelf: "center",
+                    color: "white",
+                    fontFamily: fonts.bold,
+                  }}
+                >
+                  Submit
+                </Text>
+                <Text
+                  style={{
+                    alignSelf: "center",
+                    color: "white",
+                    fontFamily: fonts.bold,
+                  }}
+                >
+                  message
+                </Text>
+              </View>
+            </TouchableWithoutFeedback>
+          </View>
         </View>
       </View>
 
