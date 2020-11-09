09f998efba97445db2426037501bd2f82f9c5452
Jenni <jenni.svensson@live.se>
2020-11-09 19:39:16 +0100

    Added signup mutation

--- graphql/mutations.js
+++ graphql/mutations.js
@@ -1,9 +1,30 @@
 import gql from "graphql-tag";
 
-
-export const LOGIN = gql`mutation login($email: String! $password: String!) {
-    login(email: $email password: $password) {
+export const LOGIN = gql`
+  mutation login($email: String!, $password: String!) {
+    login(email: $email, password: $password) {
       token
     }
-  }`
-  ;
+  }
+`;
+
+export const SIGNUP = gql`
+  mutation signup(
+    $firstName: String!
+    $lastName: String
+    $email: String!
+    $password: String!
+  ) {
+    signup(
+      signupInput: {
+        firstName: $firstName
+        lastName: $lastName
+        email: $email
+        password: $password
+      }
+    ) {
+      firstName
+      id
+    }
+  }
+`;
