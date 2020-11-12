c4a18ecade50df442f0291c2233a2ea93d26f10e
DiegoOTdC <studioartdiego@gmail.com>
2020-11-12 17:30:31 +0100

    add server or localhost option in config

--- App/config/index.js
+++ App/config/index.js
@@ -4,7 +4,7 @@ import AsyncStorage from "@react-native-community/async-storage";
 import { server } from "@env";
 
 const httpLink = createHttpLink({
-  uri: server,
+  uri: server || "http://localhost:5000/graphql",
 });
 
 const authLink = setContext(async (_, { headers }) => {
