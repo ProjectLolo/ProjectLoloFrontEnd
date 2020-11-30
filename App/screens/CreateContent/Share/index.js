58eb2b3ddfe449db4d21c5319dc2c0c5ec7c24cb
DiegoOTdC <studioartdiego@gmail.com>
2020-11-30 13:30:48 +0100

    add extra cards to share screen + add refetch kidSettings

--- App/screens/CreateContent/Share/index.js
+++ App/screens/CreateContent/Share/index.js
@@ -1,21 +1,18 @@
 import React, { useState, useEffect } from "react";
 // import axios from "axios";
-import {
-  View,
-  Text,
-  TouchableWithoutFeedback,
-  FlatList,
-} from "react-native";
+import { View, Text, TouchableWithoutFeedback, FlatList } from "react-native";
 // import NavButtons from "../../../components/NavButtons";
 import StoryCard from "../../../components/StoryCard";
 import styles from "@styles/styles";
 import NavHome from "../../../components/NavHome";
+import { tempStories } from "../tempStories.js";
 
 export default function Share({ route, navigation }) {
   const [stories, setStories] = useState(route.params.stories);
   // const title = route.params.nav.replace(/([A-Z])/g, ' $1').trim()
 
   console.log("Stories", stories);
+  console.log("params", route.params);
 
   return (
     <View
@@ -35,7 +32,7 @@ export default function Share({ route, navigation }) {
           alignItems: "center",
           width: "90%",
         }}
-        data={stories}
+        data={tempStories}
         numColumns={2}
         keyExtractor={(item) => item.id}
         renderItem={({ item }) => {
--- App/screens/CreateContent/tempStories.js
+++ App/screens/CreateContent/tempStories.js
@@ -1,197 +1,150 @@
-export const tempStories =[
-    
-    { "id":1,
-      "title":"Nickname", 
-      "createdAt":"2020-11-19T12:40:38.534Z", 
-      "updatedAt":"2020-11-19T12:40:38.534Z", 
-      "categoryId":1, 
-      "pages": [
-          {
-            "id":1,
-            "content": "Show how you make coffee or tea in the morning. What's your favourite flavour?",
-            "image": "https://raw.githubusercontent.com/ProjectLolo/ProjectLoLoStories/a366207126da4a92dff47e75d971fbafa187b7cb/App/assets/images/0.034tea_pot.svg",
-            "storyId": 1,
-            "createdAt":"2020-11-19T12:40:38.569Z", 
-            "updatedAt":"2020-11-19T12:40:38.569Z"
-          } 
-        ]
-    },
-    { "id":2,
-      "title":"Nickname", 
-      "createdAt":"2020-11-19T12:40:38.534Z", 
-      "updatedAt":"2020-11-19T12:40:38.534Z", 
-      "categoryId":6, 
-      "pages": [
-        { "id":5,
-        "content":"Hi [ name child], this is [ name sender ] and today I’m going to come up with a cool nickname. \n        I’d rather be with you today so we can come up with fun names together, but this way you can think about the names I came up with and let me know which one you like best next time we chat! Here comes the first one: \n        “assignment: record yourself pretending to be a pig/babe” \n        Can you guess which one I did? \n        Great job [ name child]! That was a babe! Do you like babe as a nickname for me?", 
-        "image":"https://raw.githubusercontent.com/ProjectLolo/ProjectLoLoStories/a366207126da4a92dff47e75d971fbafa187b7cb/App/assets/images/0.002NicknameBabe.svg", 
-        "storyId":2, 
-        "createdAt":"2020-11-19T12:40:38.569Z", 
-        "updatedAt":"2020-11-19T12:40:38.569Z"}, 
-        { "id":6, 
-          "content":"Now [ name sender ] is going to try another one. Let’s see. \n        This one is really yummy. I love eating it. How about “cutie pie”?: \n        “assignment: record yourself saying nom nom nom” pretending to eat a pie", 
-          "image":"", 
-          "storyId":2, 
-          "createdAt":"2020-11-19T12:40:38.569Z", 
-          "updatedAt":"2020-11-19T12:40:38.569Z"}, 
-        { "id":7, 
-          "content":"Now [ name sender ] is going to try a last one. \n        This one is quite sweet, maybe a bit too sweet for my taste [ name child]!\n        “assignment: pretend to be a princess and wave your royal hand”\n        Great job [ name child]! I was a princess!\n        \n        Now I’d like to tell you about [ name sender ]’s preferred nickname \n        My favourite nickname is [ assignment: add nickname ] \n        Because [ add reason for your nickname ]\n        [ optional assignment: room to personalize/add a story about your nickname ]  \n        That was all for today, [ name sender ] is off!\n        Next time I see you can you tell me about your nickname? \n        Bye bye [ name child]! [ your nickname ] is off! ", 
-          "image":"", 
-          "storyId":2, 
-          "createdAt":"2020-11-19T12:40:38.569Z", 
-          "updatedAt":"2020-11-19T12:40:38.569Z"}],
-    }, 
-    { "id": 3,
-      "title":"Baby goes to bed", 
-      "createdAt":"2020-11-19T12:40:38.534Z", 
-      "updatedAt":"2020-11-19T12:40:38.534Z", 
-      "categoryId":2, 
-      "pages": [
-        {
-          "id": 1,
-          "content":"This is a bedtime story, read to you by [ name sender ] \n        [ Name sender ] would rather be close to [ name child] , \n        but this way [ name sender ]  can still read to [ name child ]  while being apart and that is great, \n        [ Add “jaaaaaj” sound when flipping page ]",
-          "image":"https://github.com/ProjectLolo/ProjectLoLoStories/blob/development/App/assets/images/Illustratie1_Tekengebied%201.png?raw=true",
-          "storyId":3, 
-          "createdAt":"2020-11-19T12:40:38.569Z", 
-          "updatedAt":"2020-11-19T12:40:38.569Z",
-        },
-        {
-          "id":2,
-          "content": "This is a bottle of milk,\n        yummy! So nice!\n        [ Name child ]  drinks the bottle until it’s empty\n        [ add drinking sound “glu glu glu” when flipping page]  ",
-          "image":"https://github.com/ProjectLolo/ProjectLoLoStories/blob/development/App/assets/images/Illustratie2_Tekengebied%201.png?raw=true",
-          "storyId":3, 
-          "createdAt":"2020-11-19T12:40:38.569Z", 
-          "updatedAt":"2020-11-19T12:40:38.569Z",
-        },
-        {
-          "id":3,
-          "content": "This is [ Name child ] in a pram,\n        [ Name child ]  loves going for walks outside\n        Can you hear the birds whistle?\n        [ add bird sound/whistle when flipping page]  ",
-          "image":"https://github.com/ProjectLolo/ProjectLoLoStories/blob/development/App/assets/images/Illustratie3_Tekengebied%201.png?raw=true",
-          "storyId":3, 
-          "createdAt":"2020-11-19T12:40:38.569Z", 
-          "updatedAt":"2020-11-19T12:40:38.569Z",
-        },
-        {
-          "id":4,
-          "content": "This is one of my favourite toys,\n        [ Name child ]  can play with it and watch the stars,\n        they are twinkling in the sky\n        [ add twinkle sound “twinkly twinkly twinkly” when flipping page]  ",
-          "image":"https://github.com/ProjectLolo/ProjectLoLoStories/blob/development/App/assets/images/Illustratie4_Tekengebied%201.png?raw=true",
-          "storyId":3, 
-          "createdAt":"2020-11-19T12:40:38.569Z", 
-          "updatedAt":"2020-11-19T12:40:38.569Z",
-        },
-        {
-          "id":5,
-          "content": "After a long day [ Name child ]  goes to bath,\n        It’s fun to play with the bubbles and splash with the water\n        [ add bubble sound “blub blub blub” when flipping page ]  ",
-          "image":"https://github.com/ProjectLolo/ProjectLoLoStories/blob/development/App/assets/images/Illustratie5_Tekengebied%201.png?raw=true",
-          "storyId":3, 
-          "createdAt":"2020-11-19T12:40:38.569Z", 
-          "updatedAt":"2020-11-19T12:40:38.569Z",
-        },
-        {
-          "id":6,
-          "content": "Now [ Name child ]  is clean and can go to bed,\n        the bedtime story is almost finished,\n        [ add shhhhhhh sound when flipping page]  ",
-          "image":"https://github.com/ProjectLolo/ProjectLoLoStories/blob/development/App/assets/images/Illustratie6_Tekengebied%201.png?raw=true",
-          "storyId":3, 
-          "createdAt":"2020-11-19T12:40:38.569Z", 
-          "updatedAt":"2020-11-19T12:40:38.569Z",
-        },
-        {
-          "id":7,
-          "content": "Close your eyes sweet [ Name child ]  and dream beautiful dreams,\n        know you are safe and know that you’re loved",
-          "image":"https://github.com/ProjectLolo/ProjectLoLoStories/blob/development/App/assets/images/Illustratie7_Tekengebied%201.png?raw=true",
-          "storyId":3, 
-          "createdAt":"2020-11-19T12:40:38.569Z", 
-          "updatedAt":"2020-11-19T12:40:38.569Z",
-        },
-    
-      ]
-    },
-    { "id":4,
-      "title":"Animal Sounds",
-      "createdAt":"2020-11-19T12:40:38.534Z",
-      "updatedAt":"2020-11-19T12:40:38.534Z",
-      "categoryId":3,
-      "pages": [
-        { "id":1,
-        "content":"Hi [ name child], this is [ name sender ] and today I’m going to teach you about animals and what sounds they make. \n        I’d rather be with you today so we can practice together, but this way you get practice them and tell me about your favourite animal sound next time we chat! \n        Here comes the first one: \n        “assignment: record yourself pretending to be a monkey. Make huhuhuhu sounds” \n        Can you guess which one I did? \n        Great job [ name child]! That was a monkey!",
-          "image":"https://raw.githubusercontent.com/ProjectLolo/ProjectLoLoStories/a366207126da4a92dff47e75d971fbafa187b7cb/App/assets/images/0.001monkey.svg",
-          "storyId":1,
-            "createdAt":"2020-11-19T12:40:38.569Z",
-            "updatedAt":"2020-11-19T12:40:38.569Z"
-          },
-        { "id":2,
-        "content":"Now [ name sender ] is going to try another one. Let’s see. \n        This one is a scary one. It has stripes all over its body. How about this one: \n        “assignment: record yourself pretending to be a tiger. Make rawwwww sounds”\n        Great job [ name child]! That was a tiger!",
-          "image":"",
-          "storyId":1,
-            "createdAt":"2020-11-19T12:40:38.569Z",
-            "updatedAt":"2020-11-19T12:40:38.569Z"
-          },
-        { "id":3,
-          "content":"Now [ name sender ] is going to try a last one. \n        I’d like a cute and cuddly one because I just love cuddling with you [ name child]!\n        “assignment: record yourself pretending to be a puppy/dog. Make cute woef woef sounds” Great job [ name child]! That was a puppy!",
-          "image":"https://raw.githubusercontent.com/ProjectLolo/ProjectLoLoStories/9d84dd4599094fc4cdfc3c38c2f36b3137ee4bdd/App/assets/images/0.001_dog_puppy.svg",
-          "storyId":1,
-            "createdAt":"2020-11-19T12:40:38.569Z",
-            "updatedAt":"2020-11-19T12:40:38.569Z"},
-        { "id":4,
-          "content":"Now I’d like to tell you about [ name sender ]’s favourite animal. \n        My favourite animal is the [ assignment: add animal ] \n        Because [ assignment: add animal ]\n        [ optional assignment: room to personalize/add a story about that animal ]  That was all for today, [ name sender ] is off!\n        Next time I see you can you tell me about your favourite animal? \n        Bye bye [ name child]! Know that I’m always here for you!", 
-          "image":"", "storyId":1, 
-          "createdAt":"2020-11-19T12:40:38.569Z", 
-          "updatedAt":"2020-11-19T12:40:38.569Z"}]
-        },
-
-    { "id": 5,
-      "title":"Itsy Bitsy Spider", 
-      "createdAt":"2020-11-19T12:40:38.534Z", 
-      "updatedAt":"2020-11-19T12:40:38.534Z", 
-      "categoryId":4, 
-      "pages": [{
-        "id":1,
-        "content": "The itsy-bitsy spider\n        Climbed up the water spout\n        Down came the rain\n        And washed the spider out\n        Out came the sun\n        And dried up all the rain\n        And the itsy-bitsy spider\n        Climbed up the spout again",
-        "image":"",
-        "storyId":4, 
-        "createdAt":"2020-11-19T12:40:38.569Z", 
-        "updatedAt":"2020-11-19T12:40:38.569Z",
-      }
-      ]
-    },
-    { "id": 6,
-      "title":"Brush wash and comb", 
-      "createdAt":"2020-11-19T12:40:38.534Z", 
-      "updatedAt":"2020-11-19T12:40:38.534Z", 
-      "categoryId":5, 
-      "pages": [{
-        "id":1,
-        "content": "How to Brush teeth / how to wash hands (on staying alive song?) / Comb hair / Tying shoelaces ??",
-        "image":"https://raw.githubusercontent.com/ProjectLolo/ProjectLoLoStories/a366207126da4a92dff47e75d971fbafa187b7cb/App/assets/images/0.017brushteeth.svg",
-        "storyId":5, 
-        "createdAt":"2020-11-19T12:40:38.569Z", 
-        "updatedAt":"2020-11-19T12:40:38.569Z",
-      }
-      ]
-    },
-    { "id": 7,
-    "title":"Professions", 
-    "createdAt":"2020-11-19T12:40:38.534Z", 
-    "updatedAt":"2020-11-19T12:40:38.534Z", 
-    "categoryId":7, 
-    "pages": [{
-      "id":1,
-      "content": "What did you want to become when you were [name kid]'s age?",
-      "image":"https://raw.githubusercontent.com/ProjectLolo/ProjectLoLoStories/a366207126da4a92dff47e75d971fbafa187b7cb/App/assets/images/0.011JobsFirefighter.svg",
-      "storyId":6, 
-      "createdAt":"2020-11-19T12:40:38.569Z", 
-      "updatedAt":"2020-11-19T12:40:38.569Z",
-    }
-    ]
+export const tempStories = [
+  {
+    id: 1,
+    title: "Drink",
+    createdAt: "2020-11-19T12:40:38.534Z",
+    updatedAt: "2020-11-19T12:40:38.534Z",
+    categoryId: 1,
+    pages: [
+      {
+        id: 1,
+        content:
+          "Show how you make coffee or tea in the morning. What's your favourite flavour?",
+        image:
+          "https://raw.githubusercontent.com/ProjectLolo/ProjectLoLoStories/a366207126da4a92dff47e75d971fbafa187b7cb/App/assets/images/0.034tea_pot.svg",
+        storyId: 1,
+        createdAt: "2020-11-19T12:40:38.569Z",
+        updatedAt: "2020-11-19T12:40:38.569Z",
+      },
+    ],
   },
-          
-    
-]
+  {
+    id: 2,
+    title: "Nickname",
+    createdAt: "2020-11-19T12:40:38.534Z",
+    updatedAt: "2020-11-19T12:40:38.534Z",
+    categoryId: 6,
+    pages: [
+      {
+        id: 5,
+        content:
+          "Hi [ name child], this is [ name sender ] and today I’m going to come up with a cool nickname. \n        I’d rather be with you today so we can come up with fun names together, but this way you can think about the names I came up with and let me know which one you like best next time we chat! Here comes the first one: \n        “assignment: record yourself pretending to be a pig/babe” \n        Can you guess which one I did? \n        Great job [ name child]! That was a babe! Do you like babe as a nickname for me?",
+        image:
+          "https://raw.githubusercontent.com/ProjectLolo/ProjectLoLoStories/a366207126da4a92dff47e75d971fbafa187b7cb/App/assets/images/0.002NicknameBabe.svg",
+        storyId: 2,
+        createdAt: "2020-11-19T12:40:38.569Z",
+        updatedAt: "2020-11-19T12:40:38.569Z",
+      },
+      {
+        id: 6,
+        content:
+          "Now [ name sender ] is going to try another one. Let’s see. \n        This one is really yummy. I love eating it. How about “cutie pie”?: \n        “assignment: record yourself saying nom nom nom” pretending to eat a pie",
+        image: "",
+        storyId: 2,
+        createdAt: "2020-11-19T12:40:38.569Z",
+        updatedAt: "2020-11-19T12:40:38.569Z",
+      },
+      {
+        id: 7,
+        content:
+          "Now [ name sender ] is going to try a last one. \n        This one is quite sweet, maybe a bit too sweet for my taste [ name child]!\n        “assignment: pretend to be a princess and wave your royal hand”\n        Great job [ name child]! I was a princess!\n        \n        Now I’d like to tell you about [ name sender ]’s preferred nickname \n        My favourite nickname is [ assignment: add nickname ] \n        Because [ add reason for your nickname ]\n        [ optional assignment: room to personalize/add a story about your nickname ]  \n        That was all for today, [ name sender ] is off!\n        Next time I see you can you tell me about your nickname? \n        Bye bye [ name child]! [ your nickname ] is off! ",
+        image: "",
+        storyId: 2,
+        createdAt: "2020-11-19T12:40:38.569Z",
+        updatedAt: "2020-11-19T12:40:38.569Z",
+      },
+    ],
+  },
+  {
+    id: 4,
+    title: "Animal Sounds",
+    createdAt: "2020-11-19T12:40:38.534Z",
+    updatedAt: "2020-11-19T12:40:38.534Z",
+    categoryId: 3,
+    pages: [
+      {
+        id: 1,
+        content:
+          "Hi [ name child], this is [ name sender ] and today I’m going to teach you about animals and what sounds they make. \n        I’d rather be with you today so we can practice together, but this way you get practice them and tell me about your favourite animal sound next time we chat! \n        Here comes the first one: \n        “assignment: record yourself pretending to be a monkey. Make huhuhuhu sounds” \n        Can you guess which one I did? \n        Great job [ name child]! That was a monkey!",
+        image:
+          "https://raw.githubusercontent.com/ProjectLolo/ProjectLoLoStories/a366207126da4a92dff47e75d971fbafa187b7cb/App/assets/images/0.001monkey.svg",
+        storyId: 1,
+        createdAt: "2020-11-19T12:40:38.569Z",
+        updatedAt: "2020-11-19T12:40:38.569Z",
+      },
+      {
+        id: 2,
+        content:
+          "Now [ name sender ] is going to try another one. Let’s see. \n        This one is a scary one. It has stripes all over its body. How about this one: \n        “assignment: record yourself pretending to be a tiger. Make rawwwww sounds”\n        Great job [ name child]! That was a tiger!",
+        image: "",
+        storyId: 1,
+        createdAt: "2020-11-19T12:40:38.569Z",
+        updatedAt: "2020-11-19T12:40:38.569Z",
+      },
+      {
+        id: 3,
+        content:
+          "Now [ name sender ] is going to try a last one. \n        I’d like a cute and cuddly one because I just love cuddling with you [ name child]!\n        “assignment: record yourself pretending to be a puppy/dog. Make cute woef woef sounds” Great job [ name child]! That was a puppy!",
+        image:
+          "https://raw.githubusercontent.com/ProjectLolo/ProjectLoLoStories/9d84dd4599094fc4cdfc3c38c2f36b3137ee4bdd/App/assets/images/0.001_dog_puppy.svg",
+        storyId: 1,
+        createdAt: "2020-11-19T12:40:38.569Z",
+        updatedAt: "2020-11-19T12:40:38.569Z",
+      },
+      {
+        id: 4,
+        content:
+          "Now I’d like to tell you about [ name sender ]’s favourite animal. \n        My favourite animal is the [ assignment: add animal ] \n        Because [ assignment: add animal ]\n        [ optional assignment: room to personalize/add a story about that animal ]  That was all for today, [ name sender ] is off!\n        Next time I see you can you tell me about your favourite animal? \n        Bye bye [ name child]! Know that I’m always here for you!",
+        image: "",
+        storyId: 1,
+        createdAt: "2020-11-19T12:40:38.569Z",
+        updatedAt: "2020-11-19T12:40:38.569Z",
+      },
+    ],
+  },
+  {
+    id: 6,
+    title: "Brush wash and comb",
+    createdAt: "2020-11-19T12:40:38.534Z",
+    updatedAt: "2020-11-19T12:40:38.534Z",
+    categoryId: 5,
+    pages: [
+      {
+        id: 1,
+        content:
+          "How to Brush teeth / how to wash hands (on staying alive song?) / Comb hair / Tying shoelaces ??",
+        image:
+          "https://raw.githubusercontent.com/ProjectLolo/ProjectLoLoStories/a366207126da4a92dff47e75d971fbafa187b7cb/App/assets/images/0.017brushteeth.svg",
+        storyId: 5,
+        createdAt: "2020-11-19T12:40:38.569Z",
+        updatedAt: "2020-11-19T12:40:38.569Z",
+      },
+    ],
+  },
+  {
+    id: 7,
+    title: "Professions",
+    createdAt: "2020-11-19T12:40:38.534Z",
+    updatedAt: "2020-11-19T12:40:38.534Z",
+    categoryId: 7,
+    pages: [
+      {
+        id: 1,
+        content: "What did you want to become when you were [name kid]'s age?",
+        image:
+          "https://raw.githubusercontent.com/ProjectLolo/ProjectLoLoStories/a366207126da4a92dff47e75d971fbafa187b7cb/App/assets/images/0.011JobsFirefighter.svg",
+        storyId: 6,
+        createdAt: "2020-11-19T12:40:38.569Z",
+        updatedAt: "2020-11-19T12:40:38.569Z",
+      },
+    ],
+  },
+];
 
-export const share = tempStories.filter(story => story.categoryId == 1)
-export const read = tempStories.filter(story => story.categoryId == 2)
-export const teach = tempStories.filter(story => story.categoryId == 3)
-export const sing = tempStories.filter(story => story.categoryId == 4)
-export const activate = tempStories.filter(story => story.categoryId == 5)
-export const fun = tempStories.filter(story => story.categoryId == 6)
-export const memory = tempStories.filter(story => story.categoryId == 7)
+export const share = tempStories.filter((story) => story.categoryId == 1);
+export const read = tempStories.filter((story) => story.categoryId == 2);
+export const teach = tempStories.filter((story) => story.categoryId == 3);
+export const sing = tempStories.filter((story) => story.categoryId == 4);
+export const activate = tempStories.filter((story) => story.categoryId == 5);
+export const fun = tempStories.filter((story) => story.categoryId == 6);
+export const memory = tempStories.filter((story) => story.categoryId == 7);
